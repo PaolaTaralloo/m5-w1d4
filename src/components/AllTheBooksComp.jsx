426502a34e13ad8adcbc05b966d0d184cd8fe82f
PaolaTarallo <paola.trll@gmail.com>
2025-03-21 20:24:37 +0100

    border red commit

--- src/components/AllTheBooksComp.jsx
+++ src/components/AllTheBooksComp.jsx
@@ -1,43 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-// import { Card, Container, Row, Col } from 'react-bootstrap';
-import {Container, Row } from 'react-bootstrap';
+import { Container, Row, Col, Form } from 'react-bootstrap';
 import SingleBookComp from '../components/SingleBookComp.jsx';
 
 // Importa i dati dai file JSON
 import fantasyBooks from '../data/fantasy.json';
-import historyBooks from '../data/history.json';
-import horrorBooks from '../data/horror.json';
-import romanceBooks from '../data/romance.json';
-import scifiBooks from '../data/scifi.json';
+// import historyBooks from '../data/history.json';
+// import horrorBooks from '../data/horror.json';
+// import romanceBooks from '../data/romance.json';
+// import scifiBooks from '../data/scifi.json';
 
 // Unisci tutti i libri in un unico array
 const allBooks = [
   ...fantasyBooks,
-  ...historyBooks,
-  ...horrorBooks,
-  ...romanceBooks,
-  ...scifiBooks
+  // ...historyBooks,
+  // ...horrorBooks,
+  // ...romanceBooks,
+  // ...scifiBooks
 ];
 
 const AllTheBooks = () => {
-  return (
-    <Container className="mt-4, mb-5" >
-      <Row>
-        {allBooks.map((book) => (
-          <SingleBookComp key={book.asin} book={book} />
+  const [books, setBooks] = useState (allBooks)
+  const [search, setSearch] = useState ()
 
-          // <Col key={book.asin} md={3} >
-          //   <Card className="mb-4">
-          //     <Card.Img variant="top" src={book.img} alt={book.title} />
-          //     <Card.Body>
-          //       <Card.Title>{book.title}</Card.Title>
-          //     </Card.Body>
-          //   </Card>
-          // </Col>
-        ))}
-      </Row>
-    </Container>
+  const handleSearch = (event) => {
+    setSearch (event.target.value)
+    // console.log(search)
+
+    const filteredBook = allBooks.filter (book => book.title.toLowerCase().includes(event.target.value.toLowerCase()))
+    setBooks (filteredBook)
+  }
+
+  return (
+    <>
+     <Container className="mt-4, mb-3" >
+      <Form>
+        <Row>
+          <Col className='mb-3'>
+            <Form.Control placeholder="First name" onChange={handleSearch}/>
+          </Col>
+        </Row>
+      </Form>
+      </Container>
+
+      <Container className="mt-4, mb-5" >
+        <Row>
+          {books.map((book) => (
+            <SingleBookComp key={book.asin} book={book} />
+
+            // <Col key={book.asin} md={3} >
+            //   <Card className="mb-4">
+            //     <Card.Img variant="top" src={book.img} alt={book.title} />
+            //     <Card.Body>
+            //       <Card.Title>{book.title}</Card.Title>
+            //     </Card.Body>
+            //   </Card>
+            // </Col>
+          ))}
+        </Row>
+      </Container>
+    </>
   );
 };
 
--- src/components/SingleBookComp.jsx
+++ src/components/SingleBookComp.jsx
@@ -1,15 +1,35 @@
-import React from 'react';
-import { Col, Card, } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Col, Card } from 'react-bootstrap';
+import './SingleBookComp.css';
 
-export default function SingleBookComp({book}) {
-  return (
-    <Col key={book.asin} md={3} xs={6}>
-      <Card className="mb-4">
-        <Card.Img variant="top" src={book.img} />
-        <Card.Body>
-          <Card.Title>{book.title}</Card.Title>
-        </Card.Body>
-      </Card>
-    </Col>
-  )
+export default function SingleBookComp({ book }) {
+
+    const [selected, setSelected] = useState(false);
+
+    const handleClick = () => {
+        setSelected(!selected);
+    };
+
+    // Imposta la classe dinamicamente in base a selected
+    let imgClass = 'book-cover';
+    if (selected) {
+        imgClass += ' selected';
+    }
+
+    return (
+        <Col key={book.asin} md={3} xs={6} className='my-2'>
+            <Card className="mb-4 border h-100" onClick={handleClick}>
+                <Card.Img 
+                    variant="top"
+                    className={imgClass} // Usa la classe dinamica
+                    src={book.img}
+                    alt={book.title} // Aggiungi un alt per migliorare l'accessibilità
+                    
+                />
+                <Card.Body>
+                    <Card.Title>{book.title}</Card.Title>
+                </Card.Body>
+            </Card>
+        </Col>
+    );
 }
